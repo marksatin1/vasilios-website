463b9f8ecd05548e1c0867541a7e37035627b2d6
Mark Satin <msatin2009@gmail.com>
2026-01-15 19:57:38 -0500

    commented out placeholder data: causing type mismatch error

--- app/lib/placeholder-data.ts
+++ app/lib/placeholder-data.ts
@@ -1,126 +1,126 @@
-import { ImageType } from "./definitions";
+// import { ImageType } from "./definitions";
 
-export const placeholderHomepageImages: ImageType[] = [
-  {
-    id: 1,
-    url: "/tree-frog.webp",
-    width: 554,
-    height: 554,
-    description: "Tree frog 1",
-    href: "#"
-  },
-  {
-    id: 2,
-    url: "/tree-frog-1.jpg",
-    width: 1600,
-    height: 1131,
-    description: "Tree frog 2",
-    href: "#"
-  }, {
-    id: 3,
-    url: "/tree-frog-2.jpg",
-    width: 1200,
-    height: 675,
-    description: "Tree frog 3",
-    href: "#"
-  }, {
-    id: 4,
-    url: "/tree-frog-3.jpg",
-    width: 2560,
-    height: 1386,
-    description: "Tree frog 4",
-    href: "#"
-  }, {
-    id: 5,
-    url: "/tree-frog-4.jpg",
-    width: 1480,
-    height: 833,
-    description: "Tree frog 5",
-    href: "#"
-  },
-];
+// export const placeholderHomepageImages: ImageType[] = [
+//   {
+//     id: 1,
+//     url: "/tree-frog.webp",
+//     width: 554,
+//     height: 554,
+//     description: "Tree frog 1",
+//     href: "#"
+//   },
+//   {
+//     id: 2,
+//     url: "/tree-frog-1.jpg",
+//     width: 1600,
+//     height: 1131,
+//     description: "Tree frog 2",
+//     href: "#"
+//   }, {
+//     id: 3,
+//     url: "/tree-frog-2.jpg",
+//     width: 1200,
+//     height: 675,
+//     description: "Tree frog 3",
+//     href: "#"
+//   }, {
+//     id: 4,
+//     url: "/tree-frog-3.jpg",
+//     width: 2560,
+//     height: 1386,
+//     description: "Tree frog 4",
+//     href: "#"
+//   }, {
+//     id: 5,
+//     url: "/tree-frog-4.jpg",
+//     width: 1480,
+//     height: 833,
+//     description: "Tree frog 5",
+//     href: "#"
+//   },
+// ];
 
-export const placeholderCommercialsImages: ImageType[] = [
-  {
-    id: 1,
-    url: "/emu-1.jpg",
-    width: 2000,
-    height: 1157,
-    description: "Emu 1",
-    href: "/commercials/emu-1"
-  },
-  {
-    id: 2,
-    url: "/emu.webp",
-    width: 1600,
-    height: 1067,
-    description: "Emu 2",
-    href: "/commercials/emu-2"
-  }, {
-    id: 3,
-    url: "/emu-2.jpg",
-    width: 1600,
-    height: 900,
-    description: "Emu 3",
-    href: "/commercials/emu-3"
-  }, {
-    id: 4,
-    url: "/emu-3.webp",
-    width: 1365,
-    height: 1024,
-    description: "Emu 4",
-    href: "/commercials/emu-4"
-  },
-];
+// export const placeholderCommercialsImages: ImageType[] = [
+//   {
+//     id: 1,
+//     url: "/emu-1.jpg",
+//     width: 2000,
+//     height: 1157,
+//     description: "Emu 1",
+//     href: "/commercials/emu-1"
+//   },
+//   {
+//     id: 2,
+//     url: "/emu.webp",
+//     width: 1600,
+//     height: 1067,
+//     description: "Emu 2",
+//     href: "/commercials/emu-2"
+//   }, {
+//     id: 3,
+//     url: "/emu-2.jpg",
+//     width: 1600,
+//     height: 900,
+//     description: "Emu 3",
+//     href: "/commercials/emu-3"
+//   }, {
+//     id: 4,
+//     url: "/emu-3.webp",
+//     width: 1365,
+//     height: 1024,
+//     description: "Emu 4",
+//     href: "/commercials/emu-4"
+//   },
+// ];
 
-export const placeholderFeaturesImages: ImageType[] = [
-  {
-    id: 1,
-    url: "/serval.avif",
-    width: 3072,
-    height: 2048,
-    description: "Serval 1",
-    href: "/features-&-tv/serval-1"
-  },
-  {
-    id: 2,
-    url: "/serval-1.jpg",
-    width: 1400,
-    height: 790,
-    description: "Serval 2",
-    href: "/features-&-tv/serval-2"
-  }, {
-    id: 3,
-    url: "/serval-2.webp",
-    width: 2000,
-    height: 1332,
-    description: "Serval 3",
-    href: "/features-&-tv/serval-3"
-  }
-];
+// export const placeholderFeaturesImages: ImageType[] = [
+//   {
+//     id: 1,
+//     url: "/serval.avif",
+//     width: 3072,
+//     height: 2048,
+//     description: "Serval 1",
+//     href: "/features-&-tv/serval-1"
+//   },
+//   {
+//     id: 2,
+//     url: "/serval-1.jpg",
+//     width: 1400,
+//     height: 790,
+//     description: "Serval 2",
+//     href: "/features-&-tv/serval-2"
+//   }, {
+//     id: 3,
+//     url: "/serval-2.webp",
+//     width: 2000,
+//     height: 1332,
+//     description: "Serval 3",
+//     href: "/features-&-tv/serval-3"
+//   }
+// ];
 
-export const placeholderDocumentaryImages: ImageType[] = [
-  {
-    id: 1,
-    url: "/seal.webp",
-    width: 1600,
-    height: 1067,
-    description: "Elephant Seal 1",
-    href: "/documentary/seal-1"
-  },
-  {
-    id: 2,
-    url: "/seal-1.jpg",
-    width: 1300,
-    height: 731,
-    description: "Elephant Seal 2",
-    href: "/documentary/seal-2"
-  }, {
-    id: 3,
-    url: "/seal-2.jpg",
-    width: 1320,
-    height: 880,
-    description: "Elephant Seal 3",
-    href: "/documentary/seal-3"
-  }
-];
+// export const placeholderDocumentaryImages: ImageType[] = [
+//   {
+//     id: 1,
+//     url: "/seal.webp",
+//     width: 1600,
+//     height: 1067,
+//     description: "Elephant Seal 1",
+//     href: "/documentary/seal-1"
+//   },
+//   {
+//     id: 2,
+//     url: "/seal-1.jpg",
+//     width: 1300,
+//     height: 731,
+//     description: "Elephant Seal 2",
+//     href: "/documentary/seal-2"
+//   }, {
+//     id: 3,
+//     url: "/seal-2.jpg",
+//     width: 1320,
+//     height: 880,
+//     description: "Elephant Seal 3",
+//     href: "/documentary/seal-3"
+//   }
+// ];
